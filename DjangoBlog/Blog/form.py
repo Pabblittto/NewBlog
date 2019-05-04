5ec644374dd14d3c8defdeaaa0ee37ddd16e3f57
R4F44L <chojnowskigr@gmail.com>
2019-05-05 00:31:44 +0200

    dodanie wylogwanie

--- DjangoBlog/Blog/form.py
+++ DjangoBlog/Blog/form.py
@@ -1,5 +1,6 @@
 from django import forms
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 class LoginForm(forms.Form):
     UserName= forms.CharField(max_length=150)
     password= forms.CharField(widget=forms.PasswordInput())
@@ -7,19 +8,11 @@ class LoginForm(forms.Form):
 
 
 
-class RegisterForm(forms.Form):
-    UserName= forms.CharField( max_length=150)
-    Email = forms.EmailField()
-    Password= forms.CharField(
-        widget=forms.PasswordInput(),
-        label=('Password')
-
-        )
-  
-    RepeatPassword= forms.CharField(
-        widget=forms.PasswordInput(),
-        label=('Repeat Password')
-    )
+class CustomRegisterForm(UserCreationForm):
+    Opis = forms.CharField(max_length=500)
+    class Meta:
+        model = User
+        fields = ('username','email','password1','password2','Opis') 
     
     def clean_Password(self):
         password1= self.data.get('Password')
--- DjangoBlog/Blog/views.py
+++ DjangoBlog/Blog/views.py
@@ -2,21 +2,33 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from . import models
 from . import form
-from .form import LoginForm,RegisterForm
+from .form import LoginForm
+from .form import CustomRegisterForm
+
 # Create your views here.
 
 def registration(request):
     if request.method == 'POST':
-        Form = RegisterForm(request.POST)
+        Form = CustomRegisterForm(request.POST)
         if Form.is_valid():
-            messages.success(request,"Konto założone")
+            messages.success(request,'Konto założone')
             user = Form.save()
-            profil = models.Profil(User=user)
+            profil = models.Profil.objects.create(User=user,Opis='Tymczasowy Opis')
             profil.save()
             return redirect('home')
         else:
             messages.error(request,"Błąd")
             return render(request,'Blog/registration.html',{'form': Form})
     else:
-        Form = RegisterForm()
+        Form = CustomRegisterForm()
         return render(request,'Blog/registration.html',{'form': Form})
+
+def home(request):
+    posts = models.Post.objects.all()
+    return render(request,"Blog/main.html",{'posts': posts})
+def login(request):
+    if request.user.is_authenticated():
+        messages.success(request,'Zalogowano')
+        return render(request,"Blog/main.html")
+    else:
+        return render(request,"Blog/main.html")
